b9ef6fded17b7d9355020276d872787681c6758b
dharaneesh <dhara8877@gmail.com>
2022-09-14 10:38:52 +0530

    commit

--- djangoOTdetection/settings.py
+++ djangoOTdetection/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = './static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
